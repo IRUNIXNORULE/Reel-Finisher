8d0bb0febf3de9839aa2225fd89c2ab83bb910a7
KuRiGow <kansiisback@gmail.com>
2025-01-28 22:35:16 +0700

    Reel Finish.lua

---
+++ Reel Finish.lua
@@ -0,0 +1,14 @@
+---------- FREE SRC FROM KANSY ----------
+---------- Discord.gg/alchemyhub ----------
+print ("Reel Finisher By Kansy")
+print ("Free Open Source")
+---------- Reel Finish Source Code ----------
+local args = {
+    [1] = 100,
+    [2] = true
+}
+
+game:GetService("ReplicatedStorage").events:FindFirstChild("reelfinished"):FireServer(unpack(args))
+-- Not The Main Thing --
+setfpscap (1000)
+print ("This Src Is Free And Not An Auto Reel Finish")
